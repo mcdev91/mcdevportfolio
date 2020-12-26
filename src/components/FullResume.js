adf6882ac7d75a123b1470c92badb71f7cf328d8
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-12-27 01:19:02 +0200

    strange

--- src/components/FullResume.js
+++ src/components/FullResume.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./ResumePage.css";
 import { Document, Page, pdfjs } from "react-pdf";
+import GetAppIcon from '@material-ui/icons/GetApp';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default class FullResume extends Component {
